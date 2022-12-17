a899e0af9306c595242b57650d98fbecf29559c6
lunarlife <ivan.andreychenko1502@gmail.com>
2022-12-17 23:04:56 +0100

    stability version

--- UndefinedServer/UI/Elements/Text.cs
+++ UndefinedServer/UI/Elements/Text.cs
@@ -13,14 +13,16 @@ public class Text : UIElement
 {
     private Color _color;
     private string _text;
+    private readonly Rect _rect;
     private FontStyle _fontStyle;
     private TextWrapping _wraping;
     private FontSize _size;
 
-    public Text(string text, Color? color = null, FontSize? size = null, FontStyle fontStyle = FontStyle.Normal, TextWrapping? textWrapping = null, UIElement? parent = null) : base(parent)
+    public Text(Rect rect, string text, Color? color = null, FontSize? size = null, FontStyle fontStyle = FontStyle.Normal, TextWrapping? textWrapping = null, UIElement? parent = null) : base(parent)
     {
         _color = color ?? Color.Black;
         _text = text;
+        _rect = rect;
         _fontStyle = fontStyle;
         _size = size ?? new FontSize(28);
         _wraping = textWrapping ?? new TextWrapping
@@ -33,20 +35,12 @@ public class Text : UIElement
 
     public override ViewParameters CreateNewView(IUIViewer viewer) => new()
     {
-        OriginalRect = new Rect(100, 100, 100, 100)
+        OriginalRect = _rect
     };
 
     public override void OnCreateView(IUIView view)
     {
         var text = view.AddComponent<TextComponent>();
-        var mouseUp = view.AddComponent<MouseUpHandlerComponent>();
-        var mouseEnter = view.AddComponent<MouseEnterHandlerComponent>();
-        var mouseExit = view.AddComponent<MouseExitHandlerComponent>();
-        var mouseDown = view.AddComponent<MouseDownHandlerComponent>();
-        var mouseHolding = view.AddComponent<MouseHoldingHandlerComponent>();
-        mouseUp.Keys = MouseKey.Left;
-        mouseHolding.Keys = MouseKey.Right;
-        mouseDown.Keys = MouseKey.Middle;
         text.Color = _color;
         text.Text = _text;
         text.FontStyle = _fontStyle;
